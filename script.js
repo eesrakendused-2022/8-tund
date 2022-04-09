5f179d65363ad659905c08ab7731c882c3961190
Paula007 <paula@videvik.com>
2022-04-09 17:48:43 +0300

    Almost ready

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// Update the current slider value (each time you drag the slider handle)
-$('#range').change(function (e) {
-    e.preventDefault();
-    $('#age').html(this.value);
-});
-
-
-// TO-DO slaiderile on vaja muutuvad väärtused külge panna
+// kalkulaatori tulemuse arvutamine ja näitamine
 $('#pension-btn').click(function () {
     let sex = $('input[type=radio][name=sex]:checked').val();
     let age = $('input[type=range][name=age]').val();
@@ -21,6 +14,18 @@ $('#pension-btn').click(function () {
         $('#result').html('Sul on pensionini jäänud veel <strong>' + timeTillPension + '</strong> aastat!');
     }
 });
+
+// slaideri hetkeväärtuse näitaja selle liigutamise ajal
+window.onload = function () {
+    let rangeDisplay = document.getElementById('age-display');
+    let ageDisplay = document.getElementById('age');
+    let slider = document.getElementById('range');
+    rangeDisplay.innerHTML = slider.value;
+    slider.oninput = function () {
+        rangeDisplay.innerHTML = this.value;
+        ageDisplay.innerHTML = this.value;
+    };
+};
 
 // ülesannete kirjelduse toggle, https://www.w3schools.com/howto/howto_js_accordion.asp 
 let task = document.getElementsByClassName("task");
@@ -43,23 +48,49 @@ for (i = 0; i < task.length; i++) {
 }
 
 // tiimiliikmete kaartide slaider
+window.onload = function () {
+    $('.slider-btn').click(toggleCard);
+    let sliderButton = document.getElementById('forward');
+    sliderButton.addEventListener('click', function () {
+        console.log('Klikiti');
+    });
+};
 
-$('#previous').click(toggleCard);
-$('#forward').click(toggleCard);
 
-// TO-DO: TEE KORDA!!!
 function toggleCard() {
-    let currentPic = $('.displayed');
-    currentPic.removeClass('.displayed').hide();
-    currentPic.nextElementSibling.addClass('.displayed').show();
-    
-    /* if ($('#bruno').css('display', 'none')) {
-        // $('#Bruno').css('display', 'block');
-        $('#bruno').css('display', 'flex');
+    if ($('#bruno').css('display', 'none')) {
         $('#paula').css('display', 'none');
-    } else if ($('#paula').css('display', 'none')) {
+        $('#bruno').css('display', 'flex');
+    } else {
         $('#bruno').css('display', 'none');
-        $('#paula').css('display', 'flex'); 
-    }*/
+        $('#paula').css('display', 'flex');
+    }
 }
 
+// FORM VALIDATIONS
+let isValid = false;
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// arvestatud, et telefoninumbris on max 12 numbrimärki
+let phoneRegex = /^\+(\d{2,3})\)?(\d{8,9})$/;
+
+window.onload = function () {
+    $('#submit-btn').click(function (e) {
+        e.preventDefault();
+        let email = $.trim($('#email').val());
+        let phoneNumber = $.trim($('#phone').val());
+        let name = $.trim($('#name').val());
+        let message = $.trim($('#message').val());
+        // kontroll, et väljad oleksid täidetud ning epost ja telefon Regexiga sobivad
+        if (email === '' || phoneNumber === '' || name === '' || message === '') {
+            alert('Täida kõik väljad');
+        } else if (!emailRegex.test(email)) {
+            alert('E-posti aadressis on viga, paranda see palun ära!');
+        } else if (!phoneRegex.test(phoneNumber)) {
+            alert('Telefoninumbris on viga, paranda vead ja jälgi, et oleksid ainult numbrid ilma tühikuteta!');
+        } else {
+            isValid = true;
+        }
+        if (isValid) { $('#contact-form').trigger('reset'); }
+        $('#success-message').text('Teade on saadetud!').fadeOut('30000', 'linear');
+    });
+};
