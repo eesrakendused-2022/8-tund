07714ed834df889c074efa93256aeaf8c82e8a80
wemitwqw <55079623+wemitwqw@users.noreply.github.com>
2022-05-31 00:48:05 +0300

    done4today

--- js/tiimileht.js
+++ js/tiimileht.js
@@ -1,71 +1,20 @@
-$(document).ready(function () {
-    //rotation speed and timer
-    var speed = 999999999;
-    
-    var run = setInterval(rotate, speed);
-    var slides = $('.slide');
-    var container = $('#slides ul');
-    var elm = container.find(':first-child').prop("tagName");
-    var item_width = container.width();
-    var previous = 'prev'; //id of previous button
-    var next = 'next'; //id of next button
-    slides.width(item_width); //set the slides to the correct pixel width
-    container.parent().width(item_width);
-    container.width(slides.length * item_width); //set the slides container to the correct total width
-    container.find(elm + ':first').before(container.find(elm + ':last'));
-    resetSlides();
-    
-    
-    //if user clicked on prev button
-    
-    $('#buttons a').click(function (e) {
-        //slide the item
-        
-        if (container.is(':animated')) {
-            return false;
-        }
-        if (e.target.id == previous) {
-            container.stop().animate({
-                'left': 0
-            }, 1500, function () {
-                container.find(elm + ':first').before(container.find(elm + ':last'));
-                resetSlides();
-            });
-        }
-        
-        if (e.target.id == next) {
-            container.stop().animate({
-                'left': item_width * -2
-            }, 1500, function () {
-                container.find(elm + ':last').after(container.find(elm + ':first'));
-                resetSlides();
-            });
-        }
-        
-        //cancel the link behavior            
-        return false;
-        
-    });
-    
-    //if mouse hover, pause the auto rotation, otherwise rotate it    
-    container.parent().mouseenter(function () {
-        clearInterval(run);
-    }).mouseleave(function () {
-        run = setInterval(rotate, speed);
-    });
-    
-    
-    function resetSlides() {
-        //and adjust the container so current is in the frame
-        container.css({
-            'left': -1 * item_width
-        });
-    }
-    
-});
-//a simple function to click next link
-//a timer will call this function, and the rotation will begin
+let buttons = $('[data-carousel-button]')
 
-function rotate() {
-    $('#next').click();
-}
+const slides = $('[data-slides]').children()
+
+buttons.click(target => {
+    const offset = target.currentTarget.attributes[1].value === 'next' ? 1 : -1
+
+    const activeSlide = slides.closest('[data-active]')[0]
+
+    let newIndex = [...slides].indexOf(activeSlide) + offset
+    if (newIndex < 0) {newIndex = slides.length - 1}
+    if (newIndex >= slides.length) {newIndex = 0}
+
+    $(slides[newIndex]).attr('data-active', '')
+    // $(slides[newIndex]).addClass('d-none')
+    $(activeSlide).removeAttr('data-active')
+    // $(activeSlide).removeClass('d-none')
+
+})
+
