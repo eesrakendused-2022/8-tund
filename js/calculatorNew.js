78e7994cd19ddece2fe6ebf7cfa78ee351e40f44
Alek-Jaan Tsern <winlanto@tlu.ee>
2022-05-30 22:25:41 +0300

    slider

---
+++ js/calculatorNew.js
@@ -0,0 +1,27 @@
+let enteredYear = 0;
+
+$("#age").html('<p>'+enteredYear+'</p>');
+
+$("#years").click(function (){
+    enteredYear = $("#years").val();
+    $("#age").html('<p>'+enteredYear+'</p>');
+});
+
+$("#radioContainer input").click(function (){
+    let currentGender = $("input.active");
+    let selectedGender = $(this);
+    currentGender.removeClass("active");
+    selectedGender.addClass("active");
+});
+
+$("#btnInput").click(function (){
+    let gender = $("#radioContainer input.active").attr("id");
+    if (gender == "woman"){
+        $("#pensionResult").html("Pensioonini aega jäänud: ");
+        $("#pensionResult").append(document.createTextNode(-enteredYear+63));
+    }if (gender == "man"){
+        $("#pensionResult").html("Pensioonini aega jäänud: ");
+        $("#pensionResult").append(document.createTextNode(-enteredYear+65));
+    }
+});
+
