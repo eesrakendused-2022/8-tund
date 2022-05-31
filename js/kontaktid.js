67f70eea77ceeae63df82cfee004b8f18223fa71
wemitwqw <55079623+wemitwqw@users.noreply.github.com>
2022-05-31 18:03:37 +0300

    upd

--- js/kontaktid.js
+++ js/kontaktid.js
@@ -1,7 +1,27 @@
-$('#phoneNum').on('input', function() {
-	var input=$(this);
-	var re = /\+3725\d{6,7};
-	var is_email=re.test(input.val());
-	if(is_email){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}
+$('#contact').validate({
+    errorPlacement: function(error, element) {         
+        element.before(error);
+    },
+    rules: {
+        firstName: {
+            required: true,
+            minlength: 3,
+        },
+        lastName: {
+            required: true,
+            minlength: 3,
+        },
+        email: {
+            required: true,
+            pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+
+        },
+        phoneNumber: {
+            required: true,
+            pattern: /^\+3725\d{6,7}$/
+        },
+        lisaInfo: {
+            required: true,
+        }
+    }
 });
