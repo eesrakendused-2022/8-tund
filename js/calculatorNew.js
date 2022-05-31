3172353dcc0f85c7e05c7a8cb810fa94b84653f5
Alek-Jaan Tsern <winlanto@tlu.ee>
2022-05-31 17:50:21 +0300

    calc fix

--- js/calculatorNew.js
+++ js/calculatorNew.js
@@ -1,7 +1,8 @@
-let age = $('input[type="range"]').on('input', () => {
+let age = 0;
+age = $('input[type="range"]').on('input', () => {
     $('#age').html('<p>'+age.val()+' a.</p>');
-    console.log(age.val())
 });
+
 
 $("#age").html('<p>0 a.</p>');
 
@@ -16,7 +17,7 @@ $("#btnInput").click(function (){
     let gender = $("#radioContainer input.active").attr("id");
     let aastad;
     if (gender == "woman") {
-        aastad = -enteredYear + 63;
+        aastad = -age.val()+63;
         console.log(aastad);
         if (aastad > 0 && aastad != 1) {
             $("#pensionResult").html("Pensioonini aega jäänud: ");
@@ -30,7 +31,7 @@ $("#btnInput").click(function (){
             $("#pensionResult").html("Teie olete juba pensionäär! :)");
         }
     }if (gender == "man"){
-        aastad = -enteredYear+65;
+        aastad = -age.val()+65;
         console.log(aastad);
         if (aastad > 0 && aastad != 1) {
             $("#pensionResult").html("Pensioonini aega jäänud: ");
